0113b31b14a24825f956588fc22dbd065c2a6d6d
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-23 19:37:26 +0430

    converter testing

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('vim')
+from PIL import Image
+import numpy as np
+from utils.reader import read_image_file
+from converter.convert import rgb2ycbcr
+
+
+if __name__ == "__main__":
+    path = input("[Enter the file path] > ")
+    pix, w, h = read_image_file(path)
+
+    print(f'Image read: {w}x{h}')
+
+    pix = rgb2ycbcr(pix)
+
+    img = Image.fromarray((np.asarray(pix) * 255).astype(np.uint8))
+    img.show()
