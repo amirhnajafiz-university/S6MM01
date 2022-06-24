d15fea0095496ecba9c2ea5bd51148756147d17f
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-24 18:09:24 +0430

    add: subsampling

---
+++ quantization/sampling.py
@@ -0,0 +1,24 @@
+import cv2
+
+
+
+"""
+this function performs a 4:2:0 
+chromiance subsampling.
+"""
+def sample(y, cr, cb):
+    # normalizing
+    # channel values should be normalized, hence subtract 128
+    y = y - 128
+    cr = cr - 128
+    cb = cb - 128
+    # define subsampling factors in both horizontal and vertical directions
+    SSH, SSV = 2, 0
+
+    crf = cv2.boxFilter(cr, ddepth=-1, ksize=(2, 2))
+    cbf = cv2.boxFilter(cb, ddepth=-1, ksize=(2, 2))
+
+    crSub = crf[::SSV, ::SSH]
+    cbSub = cbf[::SSV, ::SSH]
+
+    return y, crSub, cbSub
