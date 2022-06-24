4439095d1c7697740e4f27882dc7c1b94005391b
amirhnajafiz <najafizadeh21@gmail.com>
2022-06-24 15:31:03 +0430

    add: dct class

--- dct/dct.py
+++ dct/dct.py
@@ -1,16 +1,39 @@
 import numpy as np
-from scipy.fftpack import dct, idct
+from scipy.fftpack import dct
 
 
 """
 This function performs a DCT2 operation
 """
-def dct2(block):
-    return dct(dct(block.T, norm='ortho').T, norm='ortho')
+def dct2(block, norm='ortho'):
+    return dct(dct(block, norm=norm, axis=0), norm=norm, axis=1)
 
 
 """
 This function performs a IDCT2 operation
 """
-def idct2(block):
-    return idct(idct(block.T, norm='ortho').T, norm='ortho')
+def idct2(block, norm='ortho'):
+    dct(dct(block, type=3, norm=norm, axis=0), type=3, norm=norm, axis=1)
+
+
+"""
+this class opertes the 2 dimention DCT
+"""
+class DCT2D():
+    """
+    constructor
+    """
+    def __init__(self, norm='ortho'):
+        self.norm = norm
+    
+    """
+    performs the dct2 operation
+    """
+    def forward(self, x):
+        return dct2(x, self.norm)
+    
+    """
+    performs the idct2 operation
+    """
+    def backward(self,x):
+        return idct2(x, self.norm)
